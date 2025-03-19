d18bc8fa855ee636ee5856b9738587b53517dc08
tjlane <thomas.joseph.lane@gmail.com>
2025-03-19 11:11:23 +0000

    nasty but working

--- refwrap/phenix.py
+++ refwrap/phenix.py
@@ -1,30 +1,26 @@
 from __future__ import annotations
 
+import re
+import shutil
 import subprocess
 import tempfile
-import gemmi
-import os
-import re
-
-from pathlib import Path
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Any
+
+import gemmi
 import reciprocalspaceship as rs
-import shutil
+from reciprocalspaceship.utils import add_rfree
 
 
 @dataclass
-class MyPhenixParams:
-    data_labels: str | None = None
+class PhenixParams:
     number_of_macrocycles: int = 3
 
     def format(self) -> list[str]:
         formatted_parameters: list[str] = [
             f"main.number_of_macro_cycles={self.number_of_macrocycles}",
         ]
-
-        if self.data_labels:
-            formatted_parameters.append(f"refinement.input.xray_data.labels={self.data_labels}")
-
         return formatted_parameters
 
 
@@ -40,16 +36,13 @@ class RefinementStats:
             r_work = float(match.group(1))
             r_free = float(match.group(2))
             return r_work, r_free
-        else:
-            raise ValueError("R values not found in the text.")
+        msg = "R values not found in the text."
+        raise ValueError(msg)
 
     @classmethod
     def read_from_log(cls, logfile: Path) -> RefinementStats:
-        with open(logfile, "r") as f:
-            logtext = logfile.read_text()
-
+        logtext = logfile.read_text()
         r_work, r_free = cls.extract_r_values(logtext)
-
         return RefinementStats(
             r_work=r_work,
             r_free=r_free,
@@ -57,24 +50,29 @@ class RefinementStats:
 
 
 def phenixrefine(
-        *,
-        pdb_file: Path,
-        mtz_file: Path,
-        phenix_params: MyPhenixParams,
+    *,
+    structure: gemmi.Structure,
+    amplitudes: rs.DataSeries,
+    uncertainties: rs.DataSeries,
+    rflags: rs.DataSeries,
+    cell: Any,
+    spacegroup: Any,
+    phenix_params: PhenixParams,
 ) -> tuple[gemmi.Structure, rs.DataSet, RefinementStats]:
-
     with tempfile.TemporaryDirectory() as tmpdir:
         tmpdir_path = Path(tmpdir)
-        os.symlink(pdb_file, tmpdir_path / 'input.pdb')
-        os.symlink(mtz_file, tmpdir_path / 'input.mtz')
 
-        cmd = [
-            "phenix.refine",
-            "input.pdb",
-            "input.mtz",
-            *phenix_params.format()
-        ]
-        
+        refinement_data = rs.DataSet(
+            {"F-obs": amplitudes, "SIGF-obs": uncertainties, "R-free-flags": rflags}
+        )
+        refinement_data.cell = cell
+        refinement_data.spacegroup = spacegroup
+        refinement_data.write_mtz(str(tmpdir_path / "input.mtz"))
+
+        structure.write_pdb(str(tmpdir_path / "input.pdb"))
+
+        cmd = ["phenix.refine", "input.pdb", "input.mtz", *phenix_params.format()]
+
         process_return = subprocess.run(cmd, cwd=tmpdir, check=False, capture_output=True)
         print(process_return)
 
@@ -85,7 +83,7 @@ def phenixrefine(
         refined_mtz = tmpdir_path / "input_refine_001.mtz"
         refinment_log = tmpdir_path / "input_refine_001.log"
 
-        shutil.copy(tmpdir_path / 'input_refine_001.log', "/Users/tjlane/Desktop")
+        shutil.copy(tmpdir_path / "input_refine_001.log", "/Users/tjlane/Desktop")
 
         assert refined_pdb.exists()
         assert refined_mtz.exists()
@@ -96,14 +94,28 @@ def phenixrefine(
         stats = RefinementStats.read_from_log(refinment_log)
 
         return structure, mtz_dataset, stats
-    
+
 
 if __name__ == "__main__":
-    p = MyPhenixParams(data_labels="FW-F,FW-SIGF", number_of_macrocycles=1)
+    p = PhenixParams(number_of_macrocycles=1)
     mtz = Path("/Users/tjlane/Desktop/ocp-xfel-bench/models/ech/10ps/light-F.mtz")
-    pdb = Path("/Users/tjlane/Desktop/ocp-xfel-bench/models/ech/10ps/10ps_extr_realspace-occ20_1.pdb")
-    
-    structure, mtz_dataset, stats = phenixrefine(pdb_file=pdb, mtz_file=mtz, phenix_params=p)
+    pdb = Path(
+        "/Users/tjlane/Desktop/ocp-xfel-bench/models/ech/10ps/10ps_extr_realspace-occ20_1.pdb"
+    )
+
+    ds = rs.read_mtz(str(mtz))
+    add_rfree(ds, fraction=0.05, ccp4_convention=False, inplace=True)
+    init_structure = gemmi.read_structure(str(pdb))
+
+    structure, mtz_dataset, stats = phenixrefine(
+        structure=init_structure,
+        amplitudes=ds["FW-F"],
+        uncertainties=ds["FW-SIGF"],
+        rflags=ds["R-free-flags"],
+        cell=ds.cell,
+        spacegroup=ds.spacegroup,
+        phenix_params=p,
+    )
 
     print(structure)
     print(mtz_dataset)
